8ba248077691cac9f05b987f1d9f39b36adfb1f4
Chris Rogers <chris@nomadicism.com>
2013-04-07 21:17:36 -0400

    Bulk commit of pre-existing files and directories

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+			$('.body').fadeIn(800);
+			});
+
+		$(window).load(function() {
+			$('.flexslider').flexslider({
+				animation: 'slide',
+				directionNav: false,
+				controlNav: false,
+			});
+		
+		});
